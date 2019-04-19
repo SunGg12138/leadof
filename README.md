c404274068753ba8bd72498f66d5086b288459c3
SunGg12138 <961154209@qq.com>
2019-04-19 18:03:57 +0800

    base

---
+++ README.md
@@ -0,0 +1,31 @@
+# Leadof
+
+解决并发时，重复请求资源的模块
+
+## 示例
+
+之前：
+```javascript
+const Leadof = require('leadof');
+
+// 获取储存在redis的热帖列表
+async function getHotTip() {
+  let ret = await redis.get('tip:hot');
+  return ret;
+}
+let hotTip = await getHotTip();
+```
+之前，每过来一次请求都会读取redis的数据
+
+现在：
+```javascript
+const Leadof = require('leadof');
+
+async function getHotTip() {
+  let ret = await redis.get('tip:hot');
+  return ret;
+}
+// 获取储存在redis的热帖列表
+await Leadof('getHotTip')(getHotTip);
+```
+现在，会把读取redis期间的请求收集起来，读完数据后再一起返回给所有的请求
---
+++ index.js
@@ -0,0 +1,37 @@
+function Leadof (id) {
+  if (Leadof.instance[id]) {
+    let instance = Leadof.instance[id];
+    return instance.exec.bind(instance);
+  }
+  if (!(this instanceof Leadof)) return new Leadof(id);
+
+  Leadof.instance[id] = this;
+
+  this._id = id;
+  this._state = 'wait';
+  this._promise = new Promise((s, j) => {
+    this._resolve = s;
+    this._reject = j;
+  });
+  return this.exec.bind(this);
+}
+
+Leadof.prototype.exec = async function(fn, ...args){
+  if (this._state === 'wait') {
+    let ret;
+    this._state = 'pending';
+    try {
+      ret = await fn.apply(null, args);
+      this._state = 'resolve';
+      this._resolve(ret);
+    } catch (error) {
+      this._state = 'reject';
+      this._reject(error);
+    }
+    // 清除保存的实例
+    Leadof.instance[this._id] = null;
+  }
+  return this._promise;
+};
+
+Leadof.instance = {};
